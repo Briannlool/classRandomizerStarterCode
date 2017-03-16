7369818db94197653db6fa4dbbc24ca65e7b70a0
Brian Chan <brian.chan080@gmail.com>
2017-03-16 20:25:28 +0000

    randomizing names

--- script.js
+++ script.js
@@ -1,5 +1,24 @@
 $(document).ready(function(){
 
+var scriptEdders= ["Paulson", "Brian", "Kevin", "Ricky"]; 
+var scriptEd= ["Joe", "Nate", "Dennis", "Chinnell"];
 
-
+$("#studentButton").click(function(){
+    var x= Math.floor(Math.random()*scriptEdders.length);
+    console.log(x)
+    $("#studentDisplay").html(scriptEdders[x]); 
+}); 
+    var students = 
+    scriptEdders.push()
+   
+   
+   
+   
+$("#teacherButton").click(function(){
+    var y= Math.floor(Math.random()*scriptEd.length); 
+    
+$("#teacherDisplay").html(scriptEd[y]);    
+}); 
+   
+    
 });

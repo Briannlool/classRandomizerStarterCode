0ce1c37d2f74cfb1a408908c34dfffc85cda1318
Brian Chan <brian.chan080@gmail.com>
2017-03-20 14:37:09 +0000

    More abstraction added

--- script.js
+++ script.js
@@ -1,24 +1,41 @@
-/* global $ */ 
+/* global $ */
+var scriptEdders= ["Paulson", "Brian", "Kevin", "Ricky"]; 
+var scriptEd = ["Joe", "Nate", "Dennis", "Chinnell"];
+
+function add_name( name, isTeacher ) {
+    if ( name.length !== 0 ) {
+        if ( isTeacher ) {
+            scriptEd.push( name );
+        }
+        else {
+            scriptEdders.push( name );
+        }
+    }
+    else {
+        alert('please give a name');
+    }
+}
+
+
+
 $(document).ready(function(){
 
-var scriptEdders= ["Paulson", "Brian", "Kevin", "Ricky"]; 
-var scriptEd= ["Joe", "Nate", "Dennis", "Chinnell"];
 
 $("#studentButton").click(function(){
     var x= Math.floor(Math.random()*scriptEdders.length);
-    console.log(x)
+    console.log(x);
     $("#studentDisplay").html(scriptEdders[x]); 
 }); 
    
    
 $("#studentclicker").click(function(){  
-    var students= $("input").val(scriptEdders.push());  
-    if ( students === "input"){
-    $("#students").html(); 
-    else (students === "")
-    $("#students").html("Error In Value"); 
-    }    
+    var students= $("#students").val();
     
+        add_name( students, false );
+        console.log( scriptEdders );
+        $("#students").html(students);
+    $('input').val('');
+    $("#studentclicker").html("Add Another Name");
 }); 
    
    
@@ -26,8 +43,17 @@ $("#studentclicker").click(function(){
 $("#teacherButton").click(function(){
     var y= Math.floor(Math.random()*scriptEd.length); 
     
-$("#teacherDisplay").html(scriptEd[y]);    
+    $("#teacherDisplay").html(scriptEd[y]);    
 }); 
    
+$("#teacherclicker").click(function(){  
+    var teacher = $("#teachers").val();
     
+        add_name( teacher, true );
+        console.log( scriptEd );
+        $("#teachers").html(teacher); 
+    $('input').val('');
+    $("#teacherclicker").html("Add Another Name"); 
+}); 
+   
 });
